5812323da1ac63adf7ffa11cfa53775667965de1
Atla004 <jaxxqwerty@gmail.com>
2026-05-25 22:20:38 -0400

    Add BibliotecasCpp folder with multiarchivo exercise and instructions

---
+++ BibliotecasCpp/multiarchivo/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "util.h"
+#include "persona.h"
+
+int main() {
+std::cout << utils::saludar("Carlos") << std::endl;
+std::cout << "Suma: " << utils::sumar(5, 3) << std::endl;
+
+    return 0;
+}
+
+
+/*
+    Persona p("Ana", 25);
+    p.mostrar();
+*/
